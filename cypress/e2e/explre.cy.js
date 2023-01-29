b60a83d802ae44c0eb55ce2cb537f510159aee04
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-01-29 10:49:08 +0530

    add cy Version v12.4.1

---
+++ cypress/e2e/explre.cy.js
@@ -0,0 +1,8 @@
+const accountSid = "ACcc5f12030fcb3ff1a316f2bcc378abfc";
+const authToken = "15b4cd61acc8e0d3ca1cba1fa7c4ed64";
+const client = require("twilio")(accountSid, authToken);
+describe("DC", () => {
+  it("TC", () => {
+    console.log("Hell");
+  });
+});
--- cypress/e2e/explre.cy.ts
+++
@@ -1,5 +0,0 @@
-describe("DEC-001", () => {
-  it("TC-001", () => {
-    cy.visit("http://www.google.com/");
-  });
-});
